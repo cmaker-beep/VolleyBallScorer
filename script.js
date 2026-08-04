8db7017f03f0a99f861668c76c98ba1c8bd4661e
cmaker-beep <cmaker@saac.qld.edu.au>
2026-08-04 15:11:29 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+<script src="https://cdn.jsdelivr.net/npm/abase-js@2
 
+<script>
+const supabaseUrl = 'https://xwadebhflihczxlhjrpg.supabase.co';
+const supabaseKey = 'sb_publishable_4eu0Bs4poL4gOiMoVFAgKA_yp7w_c83';
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+</script>

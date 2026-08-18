a3f56dca18560b3ef33f0c14e2ad48259fe530c7
cmaker-beep <cmaker@saac.qld.edu.au>
2026-08-18 14:30:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const SUPABASE_URL = "https://xwadebhflihczxlhjrpg.supabase.co";
+const SUPABASE_KEY = "sb_publishable_4eu0Bs4poL4gOiMoVFAgKA_yp7w_c83";
+
+const sb = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log("Supabase connected");
+
 let scoreA = 0;
 let scoreB = 0;
 

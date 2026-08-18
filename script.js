530f5acefde1e44839be5ee3c2e4c2de9b8d0a0f
cmaker-beep <cmaker@saac.qld.edu.au>
2026-08-18 12:36:55 +1000

    Initialize Supabase client with URL and key

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const SUPABASE_URL = "https://xwadebhflihczxlhjrpg.supabase.co";
+const SUPABASE_KEY = "sb_publishable_4eu0Bs4poL4gOiMoVFAgKA_yp7w_c83";
 
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
 let scoreA = 0;
 let scoreB = 0;
 

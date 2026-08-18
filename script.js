e119133d15305aa259fd40f53f64118b16fa2f67
cmaker-beep <cmaker@saac.qld.edu.au>
2026-08-18 13:20:27 +1000

    Implement updateDisplay function for score updates
    Added updateDisplay function to update score elements.

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ async function changeScore(team, amount) {
 async function resetScores() {
     console.log("reset");
 }
+function updateDisplay() {
+    console.log("Updating display", scoreA, scoreB);
+
+    document.getElementById("scoreA").textContent = scoreA;
+    document.getElementById("scoreB").textContent = scoreB;
+}
 
 window.changeScore = changeScore;
 window.resetScores = resetScores;
